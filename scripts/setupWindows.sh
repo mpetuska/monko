53f17064a222ff12c97e77445261973b6c1b69b0
Martynas Petuška <martynas@petuska.dev>
2021-07-18 00:19:51 +0100

    Back to MINGW

--- scripts/setupWindows.sh
+++ scripts/setupWindows.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-
-#pacman --noconfirm -Syu
-#pacman --noconfirm -S --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake
-#pacman --noconfirm -S --needed mingw-w64-x86_64-extra-cmake-modules make tar
-#pacman --noconfirm -S --needed mingw64/mingw-w64-x86_64-cyrus-sasl
+pacman --noconfirm -Syu
+pacman --noconfirm -S --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake
+pacman --noconfirm -S --needed mingw-w64-x86_64-extra-cmake-modules make tar
+pacman --noconfirm -S --needed mingw64/mingw-w64-x86_64-cyrus-sasl
 
 set -o allexport
 sudo() {
@@ -12,10 +11,10 @@ sudo() {
  $COMMAND
 }
 
-#export CMAKE_GENERATOR="MSYS Makefiles"
+export CMAKE_GENERATOR="MSYS Makefiles"
 #export CMAKE_GENERATOR="Visual Studio 16 2019 Win64"
-export CMAKE_VS_PLATFORM_NAME="x64"
-export CMAKE_VS_PLATFORM_TOOLSET_HOST_ARCHITECTURE="x64"
-export CMAKE_C_STANDARD_REQUIRED="ON"
-export CMAKE_C_STANDARD="99"
+#export CMAKE_VS_PLATFORM_NAME="x64"
+#export CMAKE_VS_PLATFORM_TOOLSET_HOST_ARCHITECTURE="x64"
+#export CMAKE_C_STANDARD_REQUIRED="ON"
+#export CMAKE_C_STANDARD="99"
 "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.build.sh"
