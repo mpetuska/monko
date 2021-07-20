bf95608cce92636454cd90c71b6894b13d6a9dad
Martynas Petuška <martynas@petuska.dev>
2021-07-20 22:21:14 +0100

    GH actions rework v2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 ROOT="$(dirname "${BASH_SOURCE[0]}")"
-case $(uname | tr '[:upper:]' '[:lower:]') in
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+case $OS in
   darwin*)  echo "OSX detected" && "${ROOT}"/setupOSX.sh ;;
   linux*)   echo "Linux detected" && "${ROOT}"/setupUbuntu.sh ;;
-  msys*)    echo "Windows detected" && "${ROOT}"/setupWindows.sh ;;
-  *)        echo "unknown OS: $OSTYPE" && exit 1 ;;
+  msys*)    echo "Windows detected (unsupported)" && exit 1 ;;
+  mingw*)    echo "Mingw detected" && "${ROOT}"/setupMingw.sh ;;
+  *)        echo "Unknown OS: $OS" && exit 1 ;;
 esac
